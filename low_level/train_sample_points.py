4ff75da1ddffa766afaaa351c132617a3aa27d13
ouyang yuheng <ouyangyuheng8@gmail.com>
2024-06-05 23:39:58 +0200

    Initial_commit

--- low_level/train_sample_points.py
+++ low_level/train_sample_points.py
@@ -24,6 +24,7 @@ def setup_device(device_name):
 
 
 def validate_pairs(q_1, pos_2):
+    q_1 = q_1.cpu().numpy()
     pos_1 = forward_kinematics(mj_model=robot_model, mj_data=robot_data, q=q_1)
     dist = np.linalg.norm(pos_1, pos_2)
     return dist > 1.2 * (puck_radius + mallet_radius)
