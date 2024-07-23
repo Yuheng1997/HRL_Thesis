41bf6e5b7361ee6de20222b57ec4165d58de75a9
ouyang yuheng <ouyangyuheng8@gmail.com>
2024-07-23 20:01:54 +0200

    fix nan problem

--- hrl_air_hockey/agents/t_sac.py
+++ hrl_air_hockey/agents/t_sac.py
@@ -154,14 +154,14 @@ class SACPlusTermination(SAC):
 
     def inv_log_p(self, state, a_true):
         try:
-            a = (a_true - self.policy._central_a) / self.policy._delta_a
+            a = (a_true - self.policy._central_a.detach().clone()) / self.policy._delta_a.detach().clone()
             a_raw = torch.atanh(a)
             mu = self.policy._mu_approximator.predict(state, output_tensor=True).detach()
             log_sigma = self.policy._sigma_approximator.predict(state, output_tensor=True).detach()
             log_sigma = torch.clamp(log_sigma, self.policy._log_std_min(), self.policy._log_std_max())
             dist = torch.distributions.Normal(mu, log_sigma.exp())
             log_p = dist.log_prob(a_raw).sum(dim=1).detach()
-            log_p -= torch.log(1. - a.pow(2) + self.policy._eps_log_prob).sum(dim=1)
+            log_p -= torch.log(1. - a.pow(2) + self.policy._eps_log_prob.detach().clone()).sum(dim=1)
         except:
             print('problem')
         return log_p
