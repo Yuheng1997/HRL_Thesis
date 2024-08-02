736efe8d4614dbe21265b6330a3833c5a4a8a08c
ouyang yuheng <ouyangyuheng8@gmail.com>
2024-08-02 23:33:48 +0200

    ori option to update beta

--- hrl_air_hockey/agents/t_sac.py
+++ hrl_air_hockey/agents/t_sac.py
@@ -230,13 +230,13 @@ class SACPlusTermination(SAC):
         # sample w_n
         option_term_mask = np.random.rand(batch_size) < _beta.squeeze(-1)
         sampled_option = option.clone()
-        sampled_option[option_term_mask, :] = torch.tensor(self.policy.draw_action(next_state)[option_term_mask, :],
-                                                           device=self.device)
+        # sampled_option[option_term_mask, :] = torch.tensor(self.policy.draw_action(next_state)[option_term_mask, :],
+        #                                                    device=self.device)
         # sample for v(s')
         expand_next_state = next_state.clone().unsqueeze(1).repeat(1, self.num_adv_sample, 1)
         expand_sampled_option = self.policy.draw_action(expand_next_state)
 
-        adv = self.adv_func(expand_next_state, expand_sampled_option, next_state, sampled_option) + 1
+        adv = self.adv_func(expand_next_state, expand_sampled_option, next_state, sampled_option) + 0.01
         adv_tensor = torch.tensor(adv, requires_grad=False, device=self.device)
 
         beta = self.termination_approximator.predict(next_state, sampled_option, output_tensor=True)
