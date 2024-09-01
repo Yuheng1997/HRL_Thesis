4ef93db9b5227e16dc02a1932ac485f52baaca43
ouyang yuheng <ouyangyuheng8@gmail.com>
2024-09-01 23:13:53 +0200

    01_09_adv_1/two_opponent/3000/against_08_28_opponent_and_baseline/0.999

--- scripts/train_t_sac_exp.py
+++ scripts/train_t_sac_exp.py
@@ -82,9 +82,8 @@ def experiment(env_name: str = 'StaticHit',
     if agent_path_list is None:
         agent_path_list = [
                            # 't_sac_2024-08-27_20-20-56/trained_opponent/parallel_seed___1/0/HitBackEnv_2024-08-27-20-21-41'
-                           # 't_sac_2024-08-28_16-02-59/parallel_seed___0/0/HitBackEnv_2024-08-28-16-03-47',
-                           # 't_sac_2024-08-27_10-24-18/parallel_seed___2/0/HitBackEnv_2024-08-27-10-59-22',
-                           't_sac_2024-08-29_12-36-32/parallel_seed___0/0/HitBackEnv_2024-08-29-12-37-09',
+                           't_sac_2024-08-28_16-02-59/parallel_seed___0/0/HitBackEnv_2024-08-28-16-03-47',
+                           # 't_sac_2024-08-29_12-36-32/parallel_seed___0/0/HitBackEnv_2024-08-29-12-37-09',
                            # 't_sac_2024-08-29_12-36-32/parallel_seed___2/0/HitBackEnv_2024-08-29-12-38-34'
                            ]
         oppponent_agent_list = [SACPlusTermination.load(get_agent_path(agent_path)) for agent_path in agent_path_list]
