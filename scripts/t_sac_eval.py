ffe6e119535de9c746e8e2e4887fe6ef72dece42
ouyang yuheng <ouyangyuheng8@gmail.com>
2024-09-03 01:05:07 +0200

    02_09_reward/adv_0.5/two_opponent/600/against_08_29_opponent_and_baseline/0.99

--- scripts/t_sac_eval.py
+++ scripts/t_sac_eval.py
@@ -37,6 +37,7 @@ def main(
         # check_point: str = 't_sac_2024-08-28_16-02-59/parallel_seed___0/0/HitBackEnv_2024-08-28-16-03-47',
         # check_point: str = 't_sac_2024-08-29_12-36-32/parallel_seed___0/0/HitBackEnv_2024-08-29-12-37-09',
         check_point: str = 't_sac_2024-08-29_12-36-32/parallel_seed___0/0/HitBackEnv_2024-08-29-12-37-09',
+        # check_point: str = 't_sac_2024-09-02_14-25-25/parallel_seed___0/0/HitBackEnv_2024-09-02-14-25-50',
         # check_point=None
 ):
     env = HitBackEnv(visual_target=True, horizon=3000, curriculum_steps=6, gamma=0.99)
