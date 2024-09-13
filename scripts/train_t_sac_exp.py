1c726e7ae3edf170a59c6abbad4b6f40f454fc07
ouyang yuheng <ouyangyuheng8@gmail.com>
2024-09-13 14:46:31 +0200

    13_09_reward_cl_2024-09-09_18-29-12

--- scripts/train_t_sac_exp.py
+++ scripts/train_t_sac_exp.py
@@ -56,7 +56,7 @@ def experiment(env_name: str = 'StaticHit',
                layer_norm: bool = False,
 
                # Continue training
-               check_point: str = 't_sac_2024-09-09_00-23-22/parallel_seed___0/0/HitBackEnv_2024-09-09-03-23-16',
+               check_point: str = 'reward_cl_2024-09-09_18-29-12/parallel_seed___0/0/HitBackEnv_2024-09-09-18-30-02',
                # check_point: str = None,
 
                # opponent agent
@@ -81,10 +81,10 @@ def experiment(env_name: str = 'StaticHit',
 
     if agent_path_list is None:
         agent_path_list = [
-                           # 't_sac_2024-08-27_20-20-56/trained_opponent/parallel_seed___1/0/HitBackEnv_2024-08-27-20-21-41'
-                           # 't_sac_2024-08-28_16-02-59/parallel_seed___0/0/HitBackEnv_2024-08-28-16-03-47',
-                           't_sac_2024-08-29_12-36-32/parallel_seed___0/0/HitBackEnv_2024-08-29-12-37-09',
-                           # 't_sac_2024-08-29_12-36-32/parallel_seed___2/0/HitBackEnv_2024-08-29-12-38-34'
+                           'reward_cl_2024-09-09_18-29-12/parallel_seed___0/0/HitBackEnv_2024-09-09-18-30-02',
+                           'two_day_cl_line_2024-09-12_00-48-29/two_day_cl_line/parallel_seed___0/0/HitBackEnv_2024-09-12-00-49-21',
+                           'two_days_origin_2024-09-11_12-59-00/two_days_origin/parallel_seed___0/0/HitBackEnv_2024-09-11-13-00-58',
+                           'two_days_selflearn_2024-09-12_01-25-49/two_days_selflearn/parallel_seed___0/0/HitBackEnv_2024-09-12-01-26-53',
                            ]
         oppponent_agent_list = [SACPlusTermination.load(get_agent_path(agent_path)) for agent_path in agent_path_list]
         baseline_agent = BaselineAgent(env.env_info, agent_id=2)
